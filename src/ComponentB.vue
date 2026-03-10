e59d1037ca425ff126e007787955854acbf476e7
Harlem Mariscal <harlemmariscal@Harlems-MacBook-Pro-M3.local>
2026-03-10 09:09:23 -0500

    Introduction to Vue components

---
+++ src/ComponentB.vue
@@ -0,0 +1,13 @@
+<template>
+    <div style="background-color: lightblue; padding-left: 10px;">
+        This is the grandchild compoent. 
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
