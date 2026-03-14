1f392d914dd567da1aa8935b4903db7d89f56d7d
Harlem Mariscal <harlemmariscal@Harlems-MacBook-Pro-M3.local>
2026-03-13 21:52:07 -0500

    Vue scoped slots

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,62 @@
 <template>
-  <AlertBox>Invalid username or password. Please try again.</AlertBox>
-  <AlertBox></AlertBox>
-  <AlertBox>
-    <h1>The request timed out. Please refresh the page and try again.</h1>
-  </AlertBox>
+  <StudentList :list="list">
+    <template #default="{ stu }">
+      <span :class="{ cursed: stu.name == 'Harry' }">
+        {{ stu.name }}
+      </span>
+    </template>
+  </StudentList>
+
+  <hr />
+
+  <el-table :data="todoList" stripe border style="width: 100%">
+    <el-table-column prop="userId" label="User ID" width="180" />
+    <el-table-column prop="id" label="ID" width="180" />
+    <el-table-column prop="title" label="Title" />
+    <el-table-column prop="completed" label="Status">
+      <template #default="slotProps">
+        <el-tag type="success" v-if="slotProps.row.completed">Completed</el-tag>
+        <el-tag type="danger" v-else>Incomplete</el-tag>
+      </template>
+    </el-table-column>
+  </el-table>
 </template>
 
 <script setup>
-import AlertBox from './AlertBox.vue'
+import { onMounted, ref } from 'vue'
+import StudentList from './StudentList.vue'
+
+const list = ref([
+  {
+    id: 1,
+    name: 'Harry'
+  },
+  {
+    id: 2,
+    name: 'Hermione'
+  },
+  {
+    id: 3,
+    name: 'Ron'
+  }
+])
+
+const todoList = ref([])
+
+async function getTodoList() {
+  const response = await fetch('https://jsonplaceholder.typicode.com/todos')
+  const data = await response.json()
+  console.log(data)
+  todoList.value = data
+}
+
+onMounted(() => {
+  getTodoList()
+})
 </script>
 
-<style lang="scss" scoped></style>
+<style scoped>
+.cursed {
+  color: red;
+}
+</style>
